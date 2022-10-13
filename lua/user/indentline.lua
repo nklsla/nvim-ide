c43bc71b4a8d885199750e960855a84ffcc19cc9
Niklas <niklaslarsson500@hotmail.com>
2022-10-13 23:04:52 +0200

    added smart indent lines

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -4,12 +4,16 @@ if not status_ok then
 	return
 end
 
+vim.cmd([[highlight IndentBlanklineContextChar guifg=#00FF00 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineContextStart guisp=#00FF00 gui=underline]])
+
 indent_blankline.setup({
 	char = "▏",
 	show_trailing_blankline_indent = false,
 	show_first_indent_level = true,
 	use_treesitter = true,
 	show_current_context = true,
+	show_current_context_start = true,
 	buftype_exclude = { "terminal", "nofile" },
 	filetype_exclude = {
 		"help",
