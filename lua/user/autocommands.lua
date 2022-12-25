699c05721c22b484de1ab34fd7d420a713965074
Niklas <niklaslarsson500@hotmail.com>
2022-12-25 15:46:49 +0100

    updated to nvim 8.1, fixed all breaking changes

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -48,6 +48,6 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 -- Format on save
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	callback = function()
-		vim.lsp.buf.formatting_sync()
+		vim.lsp.buf.format()
 	end,
 })
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -42,64 +42,65 @@ packer.init({
 -- Install your plugins here
 return packer.startup(function(use)
 	-- My plugins here
-	use({ "wbthomason/packer.nvim", commit = "00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" }) -- Have packer manage itself
-	use({ "nvim-lua/plenary.nvim", commit = "968a4b9afec0c633bc369662e78f8c5db0eba249" }) -- Useful lua functions used by lots of plugins
-	use({ "windwp/nvim-autopairs", commit = "fa6876f832ea1b71801c4e481d8feca9a36215ec" }) -- Autopairs, integrates with both cmp and treesitter
-	use({ "numToStr/Comment.nvim", commit = "80e7746e42fa685077a7941e9022308c7ad6adf8" }) -- Comment lines, "gcc"
-	use({ "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" }) -- Icons for nvim tree
-	use({ "kyazdani42/nvim-tree.lua", commit = "bdb6d4a25410da35bbf7ce0dbdaa8d60432bc243" }) -- Folder tree
-	use({ "akinsho/bufferline.nvim", commit = "c78b3ecf9539a719828bca82fc7ddb9b3ba0c353" })
-	use({ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" })
-	use({ "nvim-lualine/lualine.nvim", commit = "3362b28f917acc37538b1047f187ff1b5645ecdd" })
-	use({ "ahmedkhalf/project.nvim", commit = "541115e762764bc44d7d3bf501b6e367842d3d4f" })
-	use({ "lewis6991/impatient.nvim", commit = "969f2c5c90457612c09cf2a13fee1adaa986d350" })
-	use({ "lukas-reineke/indent-blankline.nvim", commit = "6177a59552e35dfb69e1493fd68194e673dc3ee2" })
-	use({ "kylechui/nvim-surround", commit = "d91787d5a716623be7cec3be23c06c0856dc21b8" })
-	use({ "goolord/alpha-nvim", commit = "ef27a59e5b4d7b1c2fe1950da3fe5b1c5f3b4c94" }) -- Start up menu
-	use({ "simrat39/rust-tools.nvim", commit = "99fd1238c6068d0637df30b6cee9a264334015e9" }) --Rust tools
+	-- updates are locked
+	use({ "wbthomason/packer.nvim", lock = true }) -- Have packer manage itself
+	use({ "nvim-lua/plenary.nvim", lock = true }) -- Useful lua functions used by lots of plugins
+	use({ "windwp/nvim-autopairs", lock = true }) -- Autopairs, integrates with both cmp and treesitter
+	use({ "numToStr/Comment.nvim", lock = true }) -- Comment lines, "gcc"
+	use({ "kyazdani42/nvim-web-devicons", lock = true }) -- Icons for nvim tree
+	use({ "kyazdani42/nvim-tree.lua", lock = true }) -- Folder tree
+	use({ "akinsho/bufferline.nvim", lock = true }) -- highlighting
+	use({ "moll/vim-bbye", lock = true })
+	use({ "nvim-lualine/lualine.nvim", lock = true })
+	use({ "ahmedkhalf/project.nvim", lock = true })
+	use({ "lewis6991/impatient.nvim", lock = true })
+	use({ "lukas-reineke/indent-blankline.nvim", lock = true })
+	use({ "kylechui/nvim-surround", lock = true })
+	use({ "goolord/alpha-nvim", lock = true }) -- Start up menu
+	use({ "simrat39/rust-tools.nvim", lock = true }) --Rust tools
 
 	-- Terminals and cod running
-	use({ "akinsho/toggleterm.nvim", commit = "aaeed9e02167c5e8f00f25156895a6fd95403af8" }) -- Toggle terminals within nvim
-	use({ "CRAG666/code_runner.nvim", commit = "cbdd429972d0b55161fb9bab09862e730a4a72ac" }) -- Code runner
+	use({ "akinsho/toggleterm.nvim", lock = true }) -- Toggle terminals within nvim
+	use({ "CRAG666/code_runner.nvim", lock = true }) -- Code runner
 
 	-- Colorschemes
-	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
-	use({ "lunarvim/darkplus.nvim", commit = "aa1f0a582dc897259b9cb8bebad64110da4d4cc4" })
+	use({ "folke/tokyonight.nvim", lock = true })
+	use({ "lunarvim/darkplus.nvim", lock = true })
 
 	-- cmp plugins
-	use({ "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" }) -- The completion plugin
-	use({ "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" }) -- buffer completions
-	use({ "hrsh7th/cmp-path", commit = "466b6b8270f7ba89abd59f402c73f63c7331ff6e" }) -- path completions
-	use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
-	use({ "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" })
-	use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
-	use({ "hrsh7th/cmp-nvim-lsp-signature-help", commit = "3dd40097196bdffe5f868d5dddcc0aa146ae41eb" }) -- Show current parameter while autocompleting
+	use({ "hrsh7th/nvim-cmp", lock = true }) -- The completion plugin
+	use({ "hrsh7th/cmp-buffer", lock = true }) -- buffer completions
+	use({ "hrsh7th/cmp-path", lock = true }) -- path completions
+	use({ "saadparwaiz1/cmp_luasnip", lock = true }) -- snippet completions
+	use({ "hrsh7th/cmp-nvim-lsp", lock = true })
+	use({ "hrsh7th/cmp-nvim-lua", lock = true })
+	use({ "hrsh7th/cmp-nvim-lsp-signature-help", lock = true }) -- Show current parameter while autocompleting
 
 	-- snippets
-	use({ "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" }) --snippet engine
-	use({ "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" }) -- a bunch of snippets to use
+	use({ "L3MON4D3/LuaSnip", lock = true }) --snippet engine
+	use({ "rafamadriz/friendly-snippets", lock = true }) -- a bunch of snippets to use
 
 	-- LSP
-	use({ "neovim/nvim-lspconfig", comit = "dc4dac8fcb57cfd1cde0c04a28f3f9f3c51b4cf0" }) -- enable LSP
-	use({ "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" }) -- simple to use language server installer
-	use({ "jose-elias-alvarez/null-ls.nvim", commit = "4b403d2d724f48150ded41189ae4866492a8158b" }) -- for formatters and linters
-	use({ "RRethy/vim-illuminate", commit = "c82e6d04f27a41d7fdcad9be0bce5bb59fcb78e5" })
+	use({ "neovim/nvim-lspconfig", lock = true }) -- enable LSP
+	use({ "williamboman/nvim-lsp-installer", lock = true }) -- simple to use language server installer
+	use({ "jose-elias-alvarez/null-ls.nvim", lock = true }) -- for formatters and linters
+	use({ "RRethy/vim-illuminate", lock = true })
 
 	-- Telescope
-	use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" }) -- Fuzzy finder
+	use({ "nvim-telescope/telescope.nvim", lock = true }) -- Fuzzy finder
 
 	-- Treesitter
-	use({ "nvim-treesitter/nvim-treesitter", commit = "f3c53d225ada93a99bfd818e1c40012400e2dc55" }) -- Syntax highlighting
-	use({ "p00f/nvim-ts-rainbow", commit = "620a24f6ace51d1407d186e9e94253d463d0bc3e" }) -- Coloring for nested scopes ([{}])
-	use({ "nvim-treesitter/nvim-treesitter-context", commit = "0dd5eae6dbf226107da2c2041ffbb695d9e267c1" }) -- Sticky header for functions/statements
+	use({ "nvim-treesitter/nvim-treesitter", lock = true }) -- Syntax highlighting
+	use({ "p00f/nvim-ts-rainbow", lock = true }) -- Coloring for nested scopes ([{}])
+	use({ "nvim-treesitter/nvim-treesitter-context", lock = true }) -- Sticky header for functions/statements
 
 	-- Git
-	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" }) -- Tracking git repo and give some useful git commands directly from nvim
+	use({ "lewis6991/gitsigns.nvim", lock = true }) -- Tracking git repo and give some useful git commands directly from nvim
 
 	-- DAP
-	use({ "mfussenegger/nvim-dap", commit = "d9b315a81622457cddf6875c6ac7134baa9932ce" }) -- Debug adapt protocol client
-	use({ "rcarriga/nvim-dap-ui", commit = "ce8894c586e904ce0d00fcae1a7124132810a206" }) -- Debug user interface
-	use({ "theHamsta/nvim-dap-virtual-text", commit = "2971ce3e89b1711cc26e27f73d3f854b559a77d4" }) -- Adds text for variables during debugging
+	use({ "mfussenegger/nvim-dap", lock = true }) -- Debug adapt protocol client
+	use({ "rcarriga/nvim-dap-ui", lock = true }) -- Debug user interface
+	use({ "theHamsta/nvim-dap-virtual-text", lock = true }) -- Adds text for variables during debugging
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
